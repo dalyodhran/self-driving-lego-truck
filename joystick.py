16fb4bd8ca5d4f15bd4670106eb5143edaf85743
Igors Ivanovs <iivanovs@guidewire.com>
2022-02-17 13:06:37 +0000

    change print statement

--- joystick.py
+++ joystick.py
@@ -16,19 +16,24 @@ def control():
     collect_data = False
     while 1:
         x = sys.stdin.read(1)[0]
-        print("You pressed", x)
         if x == "A":
+            print("You pressed", x)
             motor.moveForward()
         if x == "B":
+            print("You pressed", x)
             motor.stop()
         if x == "C":
+            print("You pressed", x)
             motor.turnRight()
         if x == "D":
+            print("You pressed", x)
             motor.turnLeft()
         if x == 'r':
+            print("You pressed", x)
             print('Collecting Data')
             collect_data = True
         if x == 's':
+            print("You pressed", x)
             print('Saving colected Data')
             collect_data = False
             data_collection.save_collection()
