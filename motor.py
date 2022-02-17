c6f9f64426ef95e2bc580e992cbe6fbb207f00bb
John O'Doherty <jodoherty@guidewire.com>
2022-02-17 11:50:35 +0000

    counter version of max steering

--- motor.py
+++ motor.py
@@ -6,28 +6,22 @@ import time
 motor_forward = MotorPair('B', 'C')
 motor_forward.set_default_speed(100)
 motor_lr = Motor('A')
-global_left_rotate = 0
-global_right_rotate = 0
+lr_counter = 0
 
 def moveForward():
     motor_forward.start()
 
 
-def turnLeft(deg):
-    currentDeg = motor_lr.get_aposition()
-    if(currentDeg + deg > global_left_rotate):
-        motor_lr.run_for_degrees(global_left_rotate-currentDeg)
-    else:
-        motor_lr.run_for_degrees(deg)
+def turnLeft():
+    if(lr_counter<-5):
+        motor_lr.run_for_degrees(30)
+        counter -= 1
 
 
-def turnRight(deg):
-    currentDeg = motor_lr.get_aposition()
-    if(currentDeg + deg < global_right_rotate):
-        motor_lr.run_for_degrees(global_right_rotate-currentDeg)
-    else:
-        motor_lr.run_for_degrees(deg)
-    motor_lr.run_for_degrees(deg)
+def turnRight():
+    if(lr_counter>5):
+        motor_lr.run_for_degrees(-30)
+        counter += 1
 
 
 def stop():
@@ -41,8 +35,6 @@ def current_pos():
 
 def calibrate():
     avg_center = 0
-    avg_left = 0
-    avg_right = 0
     for i in range(0, 5):
         motor_lr.run_for_degrees(360)
 
@@ -63,8 +55,6 @@ def calibrate():
         avg_center += center
 
     motor_lr.run_to_position(avg_center/5)
-    global_left_rotate = avg_left/5
-    global_right_rotate = avg_right/5
     print(motor_lr.get_aposition())
 
 
