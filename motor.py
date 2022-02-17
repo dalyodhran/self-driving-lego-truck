d034c109a34018f0bf9f7ef1dfc17ebe298c9adb
John O'Doherty <jodoherty@guidewire.com>
2022-02-17 11:31:00 +0000

    added max for turning

--- motor.py
+++ motor.py
@@ -6,7 +6,8 @@ import time
 motor_forward = MotorPair('B', 'C')
 motor_forward.set_default_speed(100)
 motor_lr = Motor('A')
-
+global_left_rotate = 0
+global_right_rotate = 0
 
 def moveForward():
     motor_forward.start()
@@ -21,6 +22,11 @@ def turnLeft(deg):
 
 
 def turnRight(deg):
+    currentDeg = motor_lr.get_aposition()
+    if(currentDeg + deg < global_right_rotate):
+        motor_lr.run_for_degrees(global_right_rotate-currentDeg)
+    else:
+        motor_lr.run_for_degrees(deg)
     motor_lr.run_for_degrees(deg)
 
 
@@ -35,15 +41,18 @@ def current_pos():
 
 def calibrate():
     avg_center = 0
-
+    avg_left = 0
+    avg_right = 0
     for i in range(0, 5):
         motor_lr.run_for_degrees(360)
 
         position_right = motor_lr.get_aposition()
+        avg_right = avg_right + position_right
         print(position_right)
 
         motor_lr.run_for_degrees(-360)
         position_left = motor_lr.get_aposition()
+        avg_left = avg_left + position_left
         print(position_left)
         offset = abs(position_right - position_left)
         if position_left > position_right:
@@ -54,6 +63,8 @@ def calibrate():
         avg_center += center
 
     motor_lr.run_to_position(avg_center/5)
+    global_left_rotate = avg_left/5
+    global_right_rotate = avg_right/5
     print(motor_lr.get_aposition())
 
 
