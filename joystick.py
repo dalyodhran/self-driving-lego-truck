09c04b156e8dc10aec93ef2a6d93732a00cbafa2
Odhran Daly <odaly064@gmail.com>
2023-04-04 23:53:40 -0700

    adding moving forward

--- joystick.py
+++ joystick.py
@@ -1,14 +1,17 @@
+import curses
 import tty
 import sys
 import termios
+
+import motor
 from python.src.collection import data_collection
 import threading
 import time
 from pydualsense import *
-import curses
 
-filedescriptors = termios.tcgetattr(sys.stdin)
-stop_threads = False
+
+# filedescriptors = termios.tcgetattr(sys.stdin)
+# stop_threads = False
 x = 0
 
 def init():
@@ -44,8 +47,8 @@ def control():
             print("You pressed", x)
             print('Collecting Data')
             stop_threads = False
-            t1 = threading.Thread(target = run, daemon = True)
-            t1.start()
+            # t1 = threading.Thread(target = run, daemon = True)
+            # t1.start()
         if x == 's':
             print("You pressed", x)
             print('Saving colected Data')
@@ -53,15 +56,26 @@ def control():
 
         x = 0
 
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, filedescriptors)
+    # termios.tcsetattr(sys.stdin, termios.TCSADRAIN, filedescriptors)
+
+
+def cross_down(state):
+    if state:
+        motor.moveForward()
+    else:
+        motor.stop()
+
+    print(f'cross {state}')
+
+
+def joystick(stateX, stateY):
+    print(f'joystick {stateX} {stateY}')
 
 
 def run_controller():
-    while True:
-        if dualsense.state.cross:
-            print("X")
 
-        print("Exit script with 'O'\n")
+    while not dualsense.state.R1:
+
         if dualsense.state.circle:
             break
 
@@ -76,5 +90,8 @@ if __name__ == '__main__':
         print(f"epoch: {time.time():.0f}")
         time.sleep(0.5)
 
+    dualsense.cross_pressed += cross_down
+    dualsense.left_joystick_changed += joystick
     run_controller()
     dualsense.close()
+
