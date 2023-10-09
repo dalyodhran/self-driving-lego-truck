bec09b109800fe6e9d1a6cb4e44f53edcfe59a9a
Odhran Daly <odhran.daly25@mail.dcu.ie>
2023-10-09 13:48:06 -0700

    Adding controller support

--- joystick.py
+++ joystick.py
@@ -1,13 +1,14 @@
 import tty
 import sys
-import termios
 import motor
 import data_collection
-import threading
 import time
+from evdev import InputDevice, categorize, ecodes
+gamepad = InputDevice('/dev/input/event0')
+import fpstimer
 
-filedescriptors = termios.tcgetattr(sys.stdin)
-stop_threads = False
+# filedescriptors = termios.tcgetattr(sys.stdin)
+# stop_threads = False
 x = 0
 
 def init():
@@ -43,8 +44,8 @@ def control():
             print("You pressed", x)
             print('Collecting Data')
             stop_threads = False
-            t1 = threading.Thread(target = run, daemon = True)
-            t1.start()
+            # t1 = threading.Thread(target = run, daemon = True)
+            # t1.start()
         if x == 's':
             print("You pressed", x)
             print('Saving colected Data')
@@ -52,10 +53,45 @@ def control():
 
         x = 0
 
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, filedescriptors)
+    # termios.tcsetattr(sys.stdin, termios.TCSADRAIN, filedescriptors)
+
+absolutes = {
+    0: 'left joystick left/right',
+    1: 'left joystick up/down'
+}
+
+CENTER = 128
+BLIND = 6
+left_joystick = [CENTER, CENTER]
+
+def update_left_joystick_position(event):
+    global left_joystick
+    if event.code == 0:
+        left_joystick[0] = value
+    elif event.code == 1:
+        left_joystick[1] = value
 
 
 if __name__ == '__main__':
     init()
-    motor.run()
-    control()
+    # motor.run()
+    # control()
+
+    print(gamepad)
+    timer = fpstimer.FPSTimer(30)
+
+    for event in gamepad.read_loop():
+        if event.type == ecodes.EV_ABS and event.code in absolutes:
+            action, value = absolutes[event.code], event.value
+
+            if event.code in [0, 1]:
+                if event.code in [0, 1]:
+                    update_left_joystick_position(event)
+
+                if (CENTER - BLIND) < event.value < (CENTER + BLIND):
+                    continue
+
+                print(f'{left_joystick}')
+                continue
+
+        timer.sleep()
